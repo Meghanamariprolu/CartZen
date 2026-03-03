84d3ea466421f6e52aee85e3bb78964048014e1f
MeghanaM <meghanamariprolu2005@gmail.com>
2026-03-03 11:49:03 +0530

    vercel website errors

--- middleware.js
+++ middleware.js
@@ -10,6 +10,6 @@ export default withAuth({
 
 export const config = {
     matcher: [
-        "/((?!api/auth|_next/static|_next/image|favicon.ico|login|register|.*\\.(?:jpg|jpeg|gif|png|svg|webp)$|$).*)",
+        "/((?!api/auth|_next/static|_next/image|favicon.ico|login|register|.*\\.(?:jpg|jpeg|gif|png|svg|webp)$).*)",
     ],
 };
